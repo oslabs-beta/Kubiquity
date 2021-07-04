c424a80877153b15c3681d3764d7a2a82d5f6a11
David Anderson <dlande000@gmail.com>
2021-07-03 21:37:22 -0400

    Add general CSS reset

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import Errors from './components/errors/Errors';
 import Pods from './components/pods/Pods';
 
+import './styles/app.css';
+
 // TODO: after MVP, convert React Class Components to Hooks.
 // TODO: after MVP, try out Typescript.
 
