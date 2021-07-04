271910bd64877dcb723d4b0dd88cb794a927756b
David Anderson <dlande000@gmail.com>
2021-07-03 22:22:59 -0400

    Add Splash display

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Errors from './components/errors/Errors';
 import Pods from './components/pods/Pods';
+import Splash from './components/splash/Splash';
 
 // TODO: after MVP, convert React Class Components to Hooks.
 // TODO: after MVP, try out Typescript.
 
-const App = () => (
-  <div>
-    <h1>Kubiquity</h1>
-    <Errors />
-    <Pods />
-  </div>
-);
+const App = () => {
+  const [
+    isSplashShowing,
+    setIsSplashShowing
+  ] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsSplashShowing(false);
+    }, 800);
+  }, []);
+
+  if (isSplashShowing) {
+    return (
+      <Splash />
+    )
+  }
+
+  return (
+    <div>
+      <h1>Kubiquity</h1>
+      <Errors />
+      <Pods />
+    </div>
+  )
+};
 
 export default App;
---
+++ src/components/splash/Splash.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Splash = () => (
+  <>
+    <h1>Kubiquity</h1>
+    <div>Developed by k8sm8s, 2021</div>
+    <div>App is loading; please wait . . . </div>
+  </>
+);
+
+export default Splash;
