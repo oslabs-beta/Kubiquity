822dda043e98d210ba81baabb98d01eb876c3a47
David Anderson <dlande000@gmail.com>
2021-07-07 18:12:54 -0400

    Add sticky header comment to Table

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -5,6 +5,7 @@ import '../../styles/table.css';
 
 // TODO: move styling to CSS. 
 // TODO: potentially incorporate filtering: https://codesandbox.io/s/github/tannerlinsley/react-table/tree/master/examples/filtering
+// TODO: potentially add sticky headers: https://github.com/GuillaumeJasmin/react-table-sticky
 
 const Table = ({ data, headers }) => {
   const {
