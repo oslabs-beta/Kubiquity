6a99d6b693d31255489c26d2a35dc259097fcabc
David Anderson <dlande000@gmail.com>
2021-07-08 11:03:53 -0400

    Minor change to Logs

--- src/components/k8sLogs/Logs.jsx
+++ src/components/k8sLogs/Logs.jsx
@@ -41,48 +41,26 @@ const LOG_HEADERS = [
   },
 ];
 
-class Logs extends React.Component {
-  // constructor(props) {
-  //   super(props);
-
-  //   this.state = {
-  //     logs: [],
-  //   };
-  // }
-
-  // componentDidMount() {
-  //   // TODO: update to not use a localhost path
-  //   // TODO: uncomment /errors and commment in /errors/test
-  //   //! fetch('http://localhost:3000/errors')
-  //   fetch('http://localhost:3000/errors/test')
-  //     .then(res => res.json())
-  //     .then(logs => this.setState({ logs }))
-  //     .catch(err => console.log(err));
-  // }
-
-  render() {
-    return (
-      <div>
-        <div className="section-headers">
-          KUBERNETES LOGS
-        </div>
-        <div
-          className="sub-header"
-          id="log-sub-header"
-        >
-          Use the Kubiquity logs to find and resolve errors. 
-        </div>
-        {this.props.logs.length ? (
-          <Table
-            data={this.props.logs}
-            headers={LOG_HEADERS}
-          />
-        ) : (
-          <Loading resource={'logs'} />
-        )}
-      </div>
-    )
-  }
-}
+const Logs = ({ logs }) => (
+  <div>
+    <div className="section-headers">
+      KUBERNETES LOGS
+    </div>
+    <div
+      className="sub-header"
+      id="log-sub-header"
+    >
+      Use the Kubiquity logs to find and resolve errors. 
+    </div>
+    {logs.length ? (
+      <Table
+        data={logs}
+        headers={LOG_HEADERS}
+      />
+    ) : (
+      <Loading resource={'logs'} />
+    )}
+  </div>
+);
 
 export default Logs;
