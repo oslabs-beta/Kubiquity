592ca2faf6c2bf26e1ffe60c6029a10b9c4d3fc5
Jeffrey Zheng <zhengj98@outlook.com>
2021-07-07 20:41:11 -0400

    Styling fixes, remove unused node packages

--- server/controllers/metricsController.js
+++ server/controllers/metricsController.js
@@ -1,11 +1,10 @@
-const { spawn } = require('child_process');
 const fetch = require('node-fetch');
 
 const metricsController = {};
 
 const PROM_URL = 'http://127.0.0.1:9090/api/v1/';
 
-metricsController.getMemory = (req, res, next) => {
+metricsController.getMemory = async (req, res, next) => {
   try {
     // create query at current time
     const currentDate = new Date().toISOString();
@@ -13,24 +12,23 @@ metricsController.getMemory = (req, res, next) => {
     query += `&start=${currentDate}&end=${currentDate}&step=1m`;
     let memArr;
     // send query to prometheus for node memory usage
-    const data = fetch(PROM_URL + query)
-      .then(data => data.json())
-      .then(results => {
-        memArr = results.data.result[0].values[0];
-        // format results and change into megabytes
-        memArr.forEach((el, ind) => {
-          if (typeof el === 'string') el = parseFloat(el);
-          mbMemory = el / 1048576;
-          memArr[ind] = Math.floor(mbMemory);
-        });
-      });
+    const data = await fetch(PROM_URL + query);
+    const results = await data.json();
+    // format results and change into megabytes
+    memArr = results.data.result[0].values[0];
+    memArr.forEach((el, ind) => {
+      if (typeof el === 'string') el = parseFloat(el);
+      mbMemory = el / 1048576;
+      memArr[ind] = Math.floor(mbMemory);
+    });
+    
     // convert array into object to send to front end
     const memObj = Object.assign({}, memArr);
-    const formattedData = [];
-    Object.entries(memObj).forEach(([key, value]) => {
-      const formattedObj = { podId: key, memory: value };
-      formattedData.push(formattedObj);
-    });
+    const formattedData = Object.entries(memObj).map(([podId, memory]) => ({
+      podId, 
+      memory
+    }));
+
     res.locals.memory = formattedData;
     return next();
   } catch (err) {
--- server/routers/metricsRouter.js
+++ server/routers/metricsRouter.js
@@ -3,7 +3,8 @@ const metricsController = require('../controllers/metricsController');
 
 const router = express.Router();
 
-router.get('/', 
+router.get(
+  '/', 
   metricsController.getMemory, 
   (req, res) => res.status(200).json(res.locals.memory)
 );
