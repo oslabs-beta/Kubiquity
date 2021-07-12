1c3d685f9885410e921929799315de9cb4651790
David Anderson <dlande000@gmail.com>
2021-07-12 13:08:06 -0400

    Incorporate server changes to backend

--- backend/controllers/metricsController.js
+++ backend/controllers/metricsController.js
@@ -1,15 +1,52 @@
-//! Jeff's work from this PR needs to be merged into this file: https://github.com/oslabs-beta/Kubiquity/pull/28
-
 const fetch = require('node-fetch');
+const cmd = require('node-cmd');
+const { spawn } = require('child_process');
 
 const PROM_URL = 'http://127.0.0.1:9090/api/v1/';
 
 const metricsController = {};
 
+let isPromUp = false;
+
+const forwardPromPort = () => (
+  new Promise((resolve, reject) => {
+    const promPodName = cmd
+      .runSync(
+        'export POD_NAME=$(kubectl get pods --all-namespaces -l "app=prometheus,component=server" -o jsonpath="{.items[0].metadata.name}") && echo $POD_NAME',
+      )
+      .data
+      .split('\n');
+      
+    const portForward = spawn('kubectl', [
+      '--namespace=prometheus',
+      'port-forward',
+      `${promPodName[0]}`,
+      '9090',
+    ]);
+
+    portForward.stdout.on('data', (data) => {
+      console.log(`stdout: ${data}`);
+      isPromUp = true;
+      resolve();
+    });
+
+    portForward.stderr.on('data', (data) => {
+      console.log(`stderr: ${data}`);
+    });
+
+    portForward.on('close', (code) => {
+      if (code === 1) console.log('Prometheus is already running...');
+      isPromUp = true;
+      resolve();
+    });
+  })
+);
+
 metricsController.getMemory = async () => {
+  if (!isPromUp) await forwardPromPort();
+
   const currentDate = new Date().toISOString();
-  let query = `query_range?query=sum(rate(node_memory_MemFree_bytes[2m]))`;
-  query += `&start=${currentDate}&end=${currentDate}&step=1m`;
+  const query = `/query_range?query=sum(rate(node_memory_MemFree_bytes[2m]))&start=${currentDate}&end=${currentDate}&step=1m`;
 
   try {
     const data = await fetch(PROM_URL + query);
