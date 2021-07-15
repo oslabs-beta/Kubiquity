722a7a4223438cb1cd5280f5729361aef4fd7b50
David Anderson <dlande000@gmail.com>
2021-07-15 15:38:53 -0400

    Add initial search

--- src/components/log/Log.jsx
+++ src/components/log/Log.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
 import { Loading } from '../';
@@ -46,27 +46,68 @@ const LOG_HEADERS = [
   },
 ];
 
-const Log = ({ log }) => (
-  <div>
-    <div className="section-headers">
-      EVENT LOG
+const Log = ({ log }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filteredLog, setFilteredLog] = useState(log);
+
+  useEffect(() => {
+    if (!searchTerm) {
+      setFilteredLog(log);
+    } else {
+      const newFilteredLog = log.filter(entry => {
+        const values = Object.values(entry);
+
+        for (const value of values) {
+          if (typeof value === 'string' && value.includes(searchTerm)) {
+            return true;
+          }
+        }
+
+        return false;
+      });
+      setFilteredLog(newFilteredLog);
+    }
+
+    // return () => {
+    //   setSearchTerm('');
+    //   setFilteredLog(log);
+    // };
+    debugger
+  }, [searchTerm]);
+
+  const handleInput = e => {
+    const { value } = e.target;
+    setSearchTerm(value);
+  };
+
+  const resetSearch = () => setSearchTerm('');
+
+  return (
+    <div>
+      <div className="section-headers">
+        EVENT LOG
+      </div>
+      <div
+        className="sub-header"
+        id="log-sub-header"
+      >
+        Use the Kubiquity event log to find and resolve errors. 
+      </div>
+      <div>
+        <input onChange={handleInput} value={searchTerm} placeholder={'Search the logs'}></input>
+        <button onClick={resetSearch}>Reset</button>
+      </div>
+      {log.length ? (
+        <Table
+          data={filteredLog}
+          headers={LOG_HEADERS}
+        />
+      ) : (
+        <Loading resource={'log'} />
+      )}
     </div>
-    <div
-      className="sub-header"
-      id="log-sub-header"
-    >
-      Use the Kubiquity event log to find and resolve errors. 
-    </div>
-    {log.length ? (
-      <Table
-        data={log}
-        headers={LOG_HEADERS}
-      />
-    ) : (
-      <Loading resource={'log'} />
-    )}
-  </div>
-);
+  )
+}
 
 Log.propTypes = {
   log: PropTypes.array.isRequired,
