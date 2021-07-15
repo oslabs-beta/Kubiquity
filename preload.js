4417f76edfdf35eff092eaa9fcde4e3417e8f8cd
David Anderson <dlande000@gmail.com>
2021-07-15 17:03:37 -0400

    Add new channels to preload

--- preload.js
+++ preload.js
@@ -3,18 +3,22 @@ const { contextBridge, ipcRenderer } = require('electron');
 const  {
   GET_LOG,
   GET_METRICS,
+  GET_CPU_USE,
   GET_LOG_TEST,
   GOT_LOG,
   GOT_METRICS,
+  GOT_CPU_USE,
   GOT_LOG_TEST,
 } = require('./utils');
 
 const VALID_CHANNELS = [
   GET_LOG,
   GET_METRICS,
+  GET_CPU_USE,
   GET_LOG_TEST,
   GOT_LOG,
   GOT_METRICS,
+  GOT_CPU_USE,
   GOT_LOG_TEST
 ];
 
