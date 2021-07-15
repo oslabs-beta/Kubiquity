6c2192a3a7a18325bc89a6e79180cb5eb465cefb
David Anderson <dlande000@gmail.com>
2021-07-15 17:02:26 -0400

    Adjust imports and exports in utils index file

--- utils/index.js
+++ utils/index.js
@@ -1,17 +1,21 @@
 const  {
   GET_LOG,
   GET_METRICS,
+  GET_CPU_USE,
   GET_LOG_TEST,
   GOT_LOG,
   GOT_METRICS,
+  GOT_CPU_USE,
   GOT_LOG_TEST,
 } = require('./electronChannels');
 
 module.exports = {
   GET_LOG,
   GET_METRICS,
+  GET_CPU_USE,
   GET_LOG_TEST,
   GOT_LOG,
   GOT_METRICS,
+  GOT_CPU_USE,
   GOT_LOG_TEST,
 };
