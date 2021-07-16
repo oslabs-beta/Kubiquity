b188ea486a271978c3703675258d760e5b8c1690
David Anderson <dlande000@gmail.com>
2021-07-16 16:16:54 -0400

    Add file loader and logo

--- webpack.common.js
+++ webpack.common.js
@@ -46,6 +46,14 @@ module.exports = {
         test: [/\.s[ac]ss$/i, /\.css$/i],
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
     ],
   },
   resolve: {
