585524ffbc082135b0cd744ba2aca29226f98cbc
David Zhang <davidzhang@Davids-MacBook-Air.local>
2021-07-19 11:23:40 -0400

    Format enzyme

--- __tests__/enzyme.js
+++ __tests__/enzyme.js
@@ -2,47 +2,46 @@
  * @jest-environment jsdom
  */
 
-
 import React from 'react';
 import { configure, shallow, mount, render } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-// Enzyme is a wrapper around React test utilities which makes it easier to
-// shallow render and traverse the shallow rendered tree.
 import NavBar from '../src/components/navBar';
 import BarChart from '../src/components/barChart';
 
 configure({ adapter: new Adapter() });
 
 describe('React unit tests', () => {
-    let wrapper;
-    describe('NavBar component', () => {
-        const props = {
-            setIsLogShowing: jest.fn(() => console.log('setIsLogShowing')),
-            setAreMetricsShowing: jest.fn(() => console.log('setAreMetricsShowing')),
-            setIsAboutShowing: jest.fn(() => console.log('setIsAboutShowing')),
-        };
-        beforeAll(() => {
-            wrapper = shallow(<NavBar {...props} />);
-        });
+  let wrapper;
 
-        it('should render an unordered list of buttons', () => {
-            expect(wrapper.find('ul').length).toBe(1);
-        });
-            
-    });
-    describe('BarChart component', () => {
-        let wrapper;
-        const props = {
-            data: [],
-            categories: [],
-        }
-        beforeAll(() => {
-            wrapper = shallow(<BarChart {...props} />);
-        });
+  describe('NavBar component', () => {
+    const props = {
+      setIsLogShowing: jest.fn(() => console.log('setIsLogShowing')),
+      setAreMetricsShowing: jest.fn(() => console.log('setAreMetricsShowing')),
+      setIsAboutShowing: jest.fn(() => console.log('setIsAboutShowing')),
+    };
 
-        it('should render without errors', () => {
-            expect(wrapper.length).toBe(1);
-        });
+    beforeAll(() => {
+      wrapper = shallow(<NavBar {...props} />);
     });
+
+    it('should render an unordered list of buttons', () => {
+      expect(wrapper.find('ul').length).toBe(1);
+    });
+  });
+
+  describe('BarChart component', () => {
+    const props = {
+      data: [],
+      categories: [],
+    };
+
+    beforeAll(() => {
+      wrapper = shallow(<BarChart {...props} />);
+    });
+
+    it('should render without errors', () => {
+      expect(wrapper.length).toBe(1);
+    });
+  });
 });
